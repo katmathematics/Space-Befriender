8aec77735158115b49094b70ef2aa2b0c8834d4c
Katja Mathesius <67726169+katmathematics@users.noreply.github.com>
2023-07-08 00:15:44 -0500

    Reset at Bottom of Screen
    Player controller resets the player to the top of the screen when they reach the bottom.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5;
+    public float dropDistance = 1;
+
     private float hInput;
+
+    private Vector3 startingPos = new Vector3(0,3,0);
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    private bool rightNext = true;
+
+    public GameMaster gm;
 
     private void Awake() {
     }
@@ -13,7 +24,9 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.size.y / 2;
     }
 
     // Update is called once per frame
@@ -21,5 +34,34 @@ public class PlayerController : MonoBehaviour
         hInput = Input.GetAxisRaw("Horizontal");
 
         transform.Translate(Vector2.right * hInput * moveSpeed * Time.deltaTime);
+    }
+
+    void LateUpdate() {
+        Vector3 viewPos = transform.position;
+
+        viewPos = CheckLayer(viewPos);
+
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * - 1 + (objectWidth + .5f), screenBounds.x - (objectWidth + .5f));
+        
+        if (viewPos.y < screenBounds.y * - 1 + (objectHeight + 2)) {
+            transform.position = startingPos;
+            rightNext = true
+        }
+        else {
+            transform.position = viewPos;
+        }
+    }
+
+    private Vector3 CheckLayer(Vector3 viewPos){
+        if (rightNext && screenBounds.x - (objectWidth + .5f) <= viewPos.x) {
+            viewPos.y = viewPos.y - dropDistance;
+            rightNext = false;
+        }
+        else if (!rightNext && screenBounds.x * - 1 + (objectWidth + .5f) >= viewPos.x) {
+            viewPos.y = viewPos.y - dropDistance;
+            rightNext = true;
+        }
+
+        return viewPos;
     }
 }
